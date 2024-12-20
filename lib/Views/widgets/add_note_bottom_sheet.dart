d079ed2ed74d18ca3092936c4dc87ccfd1b41bd1
MuhamedElfdawy <m.elfdawy500@gmail.com>
2024-12-21 01:18:49 +0200

    fix show bottom sheet with keyboard

--- lib/Views/widgets/add_note_bottom_sheet.dart
+++ lib/Views/widgets/add_note_bottom_sheet.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes/cubits/add_note_cubit/add_note_cubit.dart';
@@ -11,7 +12,11 @@ class CustomAddNoteSheet extends StatelessWidget {
     return BlocProvider(
       create: (context) => AddNoteCubit(),
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+        padding:EdgeInsets.only(
+            right: 16,
+            left: 16,
+            top: 24,
+            bottom: MediaQuery.of(context).viewInsets.bottom),
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
           listener: (context, state) {
             if (state is AddNoteFailure) {
@@ -20,11 +25,11 @@ class CustomAddNoteSheet extends StatelessWidget {
               Navigator.pop(context);
             }
           },
-          builder: (context,state) {
-            return  AbsorbPointer(
+          builder: (context, state) {
+            return AbsorbPointer(
               absorbing: state is AddNoteLoading ? true : false,
               child: const SingleChildScrollView(
-                child:  AddNoteForm(),
+                child: AddNoteForm(),
               ),
             );
           },
