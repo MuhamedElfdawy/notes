5bc6d263e8190aa6b5f7b772b3c5311672bcd901
MuhamedElfdawy <m.elfdawy500@gmail.com>
2024-12-21 23:03:45 +0200

    create fetch notes method

--- lib/Views/notes_view.dart
+++ lib/Views/notes_view.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes/constants.dart';
 import 'package:notes/Views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes/Views/widgets/notes_view_body.dart';
+import 'package:notes/cubits/notes_cubit/notes_cubit.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (context) => const CustomAddNoteSheet(),
-          );
-        },
-        backgroundColor: primaryColor,
-        child: const Icon(Icons.add,color: Colors.black,),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) => const CustomAddNoteSheet(),
+            );
+          },
+          backgroundColor: primaryColor,
+          child: const Icon(Icons.add, color: Colors.black,),
+        ),
       ),
     );
   }
--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes/constants.dart';
 import 'package:notes/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() async {
+    try {
+      var noteBox = Hive.box<NoteModel>(notesBox);
+      emit(NotesSuccess(noteBox.values.toList()));
+    } catch (e){
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
