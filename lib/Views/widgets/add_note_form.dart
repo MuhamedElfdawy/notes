02c5f9197283544b7d14e2020e37277290cb4e7b
MuhamedElfdawy <m.elfdawy500@gmail.com>
2024-12-22 23:51:41 +0200

    format date time

--- lib/Views/widgets/add_note_form.dart
+++ lib/Views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes/models/note_model.dart';
 import 'custom_button.dart';
@@ -47,12 +48,14 @@ class _AddNoteFormState extends State<AddNoteForm> {
               return CustomButton(
                 isLoading: state is AddNoteLoading ? true : false,
                 onTap: () {
+                  DateTime now = DateTime.now();
+                  String formattedDate = DateFormat('yyyy-MM-dd').format(now);
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
                     var noteModel = NoteModel(
                       title: title!,
                       subTitle: subTitle!,
-                      date: DateTime.now().toString(),
+                      date: formattedDate,
                       color: Colors.blue.value,
                     );
                     BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
